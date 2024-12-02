0f8e539153f3ca790e638a8c5791d38e07d973f0
Drogan Cintam <drogancintam@gmail.com>
2024-12-02 16:26:47 +0000

    Add check for ARM architecture

--- libkiml/include/kiml/endian.hpp
+++ libkiml/include/kiml/endian.hpp
@@ -67,7 +67,8 @@
    || defined(__amd64__) || defined(_M_AMD64) \
    || defined(__x86_64) || defined(__x86_64__) \
    || defined(_M_X64) || defined(__bfin__) \
-   || defined(ANDROID)
+   || defined(ANDROID) \
+   || defined(__aarch64__) || defined(__ARM_ARCH)
 
 # define BOOST_LITTLE_ENDIAN
 # define BOOST_BYTE_ORDER 1234
